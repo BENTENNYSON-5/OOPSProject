b4bc31575084ecf392334fa8bbbed91ed42d1405
BENTENNYSON-5 <mail2dskkarthik@gmail.com>
2020-11-22 15:57:29 +0530

    change3

--- app/src/main/java/com/example/ksagtracker/Dashboard.java
+++ app/src/main/java/com/example/ksagtracker/Dashboard.java
@@ -1,10 +1,5 @@
 package com.example.ksagtracker;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Button;
