65381025ab13386e7bd8e3828d1b66d1bbd40221
BENTENNYSON-5 <mail2dskkarthik@gmail.com>
2020-11-22 13:38:37 +0530

    hello

---
+++ app/src/main/java/com/example/ksagtracker/AddWork.java
@@ -0,0 +1,14 @@
+package com.example.ksagtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddWork extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_work);
+    }
+}
