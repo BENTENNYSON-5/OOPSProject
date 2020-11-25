ef04a58c2be8692f3f32b6c8991288d244fbf946
Supersold123 <akhillohithdoni@gmail.com>
2020-11-25 12:46:38 +0530

    notes_by_super_V 1.0

--- app/src/main/java/com/example/ksagtracker/Notes.java
+++ app/src/main/java/com/example/ksagtracker/Notes.java
@@ -3,12 +3,32 @@ package com.example.ksagtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Notes extends AppCompatActivity {
+
+    Button save;
+    String save_toast = "Saved";
+    EditText n;
+    String notes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        n = (EditText)findViewById(R.id.Notes_text);
+        save = (Button)findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notes = n.getText().toString();
+                Toast.makeText(getApplicationContext(),save_toast,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
