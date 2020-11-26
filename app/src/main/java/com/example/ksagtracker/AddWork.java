3ffa639f3d98a2ed71ac66815ea8b023bc32dfdd
shivaharshith <shiva.harshith2000@gmail.com>
2020-11-26 19:24:30 +0530

    change 28

--- app/src/main/java/com/example/ksagtracker/AddWork.java
+++ app/src/main/java/com/example/ksagtracker/AddWork.java
@@ -3,6 +3,7 @@ package com.example.ksagtracker;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.app.AlarmManager;
@@ -14,6 +15,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -28,7 +30,8 @@ public class AddWork extends AppCompatActivity  implements TimePickerDialog.OnTi
      EditText Work;
      EditText Date;
      EditText Desc;
-     TextView mTextView;
+     private TextView mTextView;
+     Calendar c;
      Button ok;
      DatabaseReference reff;
     @Override
@@ -37,6 +40,7 @@ public class AddWork extends AppCompatActivity  implements TimePickerDialog.OnTi
         setContentView(R.layout.activity_add_work);
         mTextView = findViewById(R.id.time);
         Button buttonTimePicker =(Button) findViewById(R.id.button);
+        Button buttonDate = (Button) findViewById(R.id.button12);
         Work = findViewById(R.id.Work);
         Date = findViewById(R.id.Date);
         Desc = findViewById(R.id.Desc);
@@ -47,6 +51,13 @@ public class AddWork extends AppCompatActivity  implements TimePickerDialog.OnTi
             public void onClick(View v) {
                 ob = new SearchObjects(Work.getText(),Date.getText(),Desc.getText());
 
+            }
+        });
+        buttonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
             }
         });
         buttonTimePicker.setOnClickListener(new View.OnClickListener() {
@@ -65,9 +76,19 @@ public class AddWork extends AppCompatActivity  implements TimePickerDialog.OnTi
         });
     }
     @RequiresApi(api = Build.VERSION_CODES.N)
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+        TextView textView = (TextView) findViewById(R.id.Dates);
+        textView.setText(currentDateString);
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        Calendar c = Calendar.getInstance();
         c.set(Calendar.HOUR_OF_DAY, hourOfDay);
         c.set(Calendar.MINUTE, minute);
         c.set(Calendar.SECOND, 0);
@@ -85,9 +106,6 @@ public class AddWork extends AppCompatActivity  implements TimePickerDialog.OnTi
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, AlertReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
-        if (c.before(Calendar.getInstance())) {
-            c.add(Calendar.DATE, 1);
-        }
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
     }
     private void cancelAlarm() {
