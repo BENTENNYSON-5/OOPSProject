6dea461293cfbbc1348f977198a44e349f1cf394
shivaharshith <shiva.harshith2000@gmail.com>
2020-11-26 11:09:47 +0530

    change 25

--- app/src/main/java/com/example/ksagtracker/moreoptions.java
+++ app/src/main/java/com/example/ksagtracker/moreoptions.java
@@ -9,6 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.DialogFragment;
+import android.content.Context;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.icu.util.Calendar;
+import android.os.Build;
+
+
+import java.text.DateFormat;
 
 public class moreoptions extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 Button todo;
@@ -16,6 +26,7 @@ Button notes;
 Button logout;
 Button dailytime;
 TextView timing;
+    private TextView mTextView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +61,44 @@ TextView timing;
 
             }
         });
+        mTextView3 = findViewById(R.id.timing);
+        Button buttonTimePicker =(Button) findViewById(R.id.dailytime);
+        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+
+
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+        updateTimeText(c);
+        startAlarm(c);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void updateTimeText(Calendar c) {
+        String timeText = "Alarm set for: ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+        mTextView3.setText(timeText);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void startAlarm(Calendar c) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+        if (c.before(Calendar.getInstance())) {
+            c.add(Calendar.DATE, 1);
+        }
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
     }
 
     public void startMainActivity() {
@@ -57,12 +106,11 @@ TextView timing;
         startActivity(i);
     }
 
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        timing.setText( hourOfDay + ":" + minute);
-    }
+
+
     public void openNotes(){
         Intent i = new Intent(this,Notes.class);
         startActivity(i);
     }
+
 }
